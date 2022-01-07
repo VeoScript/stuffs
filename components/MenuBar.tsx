32e204591ce40fb6f4596449c023a84fe94101e8
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-01-07 16:21:24 +0800

    feat: add menubar and sidebar component

---
+++ components/MenuBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MenuBar = () => {
+  return (
+    <div className="flex flex-row items-center justify-between w-full p-3 border-b border-gray-200">
+      <span>Left</span>
+      <span>Right</span>
+    </div>
+  )
+}
+
+export default MenuBar
