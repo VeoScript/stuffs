e0857bc842d76bae4adee567a4caf4a7f49d9c17
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-01-08 11:25:13 +0800

    feat: add dark mode switch and effects (#4)

--- components/MenuBar.tsx
+++ components/MenuBar.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import Link from 'next/link'
+import { useTheme } from 'next-themes'
 import { RiLayoutGridFill, RiQuillPenFill, RiAccountPinCircleFill, RiMoonFill, RiSunFill } from 'react-icons/ri'
 
 const MenuBar = () => {
+
+  const {theme, setTheme} = useTheme()
+
   return (
-    <div className="flex flex-row items-center justify-between w-full px-3 py-1 border-b border-gray-200">
+    <div className="flex flex-row items-center justify-between w-full px-3 py-1 border-b border-gray-200 dark:border-gray-700">
       <div className="flex items-center justify-start w-full space-x-2">
-        <p className="font-light text-xs">Logged in as:</p>
-        <p className="font-semibold text-sm">Unknown</p>
+        <p className="font-light text-[10px]">Logged in as:</p>
+        <p className="font-semibold text-xs">Unknown</p>
       </div>
       <div className="flex items-center justify-center w-full">
         <Link href="/">
           <a title="Home">
-            <RiLayoutGridFill className="w-6 h-6 transition ease-in-out duration-200 hover:scale-95 text-gray-600 dark:text-gray-100" />
+            <RiLayoutGridFill className="w-5 h-5 transition ease-in-out duration-200 hover:scale-95 text-gray-600 dark:text-gray-100" />
           </a>
         </Link>
       </div>
@@ -27,11 +31,19 @@ const MenuBar = () => {
             <RiAccountPinCircleFill className="w-5 h-5 transition ease-in-out duration-200 hover:scale-95 text-gray-600 dark:text-gray-100" />
           </a>
         </Link>
-        <Link href="/">
-          <a title="Dark Mode">
-            <RiMoonFill className="w-5 h-5 transition ease-in-out duration-200 hover:scale-95 text-gray-600 dark:text-gray-100" />
-          </a>
-        </Link>
+        {theme !== undefined && (
+          <button
+            title="Theme Toggle"
+            onClick={() => {
+              setTheme(theme === 'dark' ? 'light' : 'dark')
+            }}
+          >
+            {theme === 'dark'
+              ? <RiSunFill className="w-5 h-5 transition ease-in-out duration-200 hover:scale-95 text-gray-600 dark:text-gray-100" />
+              : <RiMoonFill className="w-5 h-5 transition ease-in-out duration-200 hover:scale-95 text-gray-600 dark:text-gray-100" />
+            }
+          </button>
+        )}
       </div>
     </div>
   )
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const SideBar = () => {
   return (
-    <div className="flex flex-col justify-center w-full max-w-sm h-full space-y-10 overflow-y-auto border-r border-gray-200">
+    <div className="flex flex-col justify-center w-full max-w-sm h-full space-y-10 overflow-y-auto border-r border-gray-200 dark:border-gray-700">
       <div className="flex flex-col items-center justify-center w-full space-y-2">
         <h1 className="font-licorice font-black text-5xl">Stuffs</h1>
         <h6 className="font-light text-xs">I.T. Monthly Reports</h6>
       </div>
       <div className="flex flex-col w-full px-3 space-y-3">
-        <div className="flex flex-col w-full">
-          <label className="font-light text-sm text-gray-700" htmlFor="txtyear">Select Year</label>
-          <select className="cursor-pointer w-full px-3 py-2 bg-[#ebebeb] outline-none" name="txtyear" id="txtyear">
+        <div className="flex flex-col w-full space-y-1">
+          <label className="font-light text-xs text-gray-700 dark:text-gray-200" htmlFor="txtyear">Select Year</label>
+          <select className="cursor-pointer w-full px-3 py-2 bg-purple-200 dark:bg-gray-900 outline-none" name="txtyear" id="txtyear">
             <option value=""></option>
             <option value="2022">2022</option>
             <option value="2022">2023</option>
@@ -18,19 +18,19 @@ const SideBar = () => {
             <option value="2022">2025</option>
           </select>
         </div>
-        <div className="grid grid-cols-4 gap-1">
-          <button className="flex items-center justify-center w-full p-3 font-bold text-sm transition ease-in-out duration-200 bg-purple-200 hover:bg-purple-300">JAN</button>
-          <button className="flex items-center justify-center w-full p-3 font-bold text-sm transition ease-in-out duration-200 bg-purple-200 hover:bg-purple-300">FEB</button>
-          <button className="flex items-center justify-center w-full p-3 font-bold text-sm transition ease-in-out duration-200 bg-purple-200 hover:bg-purple-300">MAR</button>
-          <button className="flex items-center justify-center w-full p-3 font-bold text-sm transition ease-in-out duration-200 bg-purple-200 hover:bg-purple-300">APR</button>
-          <button className="flex items-center justify-center w-full p-3 font-bold text-sm transition ease-in-out duration-200 bg-purple-200 hover:bg-purple-300">MAY</button>
-          <button className="flex items-center justify-center w-full p-3 font-bold text-sm transition ease-in-out duration-200 bg-purple-200 hover:bg-purple-300">JUN</button>
-          <button className="flex items-center justify-center w-full p-3 font-bold text-sm transition ease-in-out duration-200 bg-purple-200 hover:bg-purple-300">JUL</button>
-          <button className="flex items-center justify-center w-full p-3 font-bold text-sm transition ease-in-out duration-200 bg-purple-200 hover:bg-purple-300">AUG</button>
-          <button className="flex items-center justify-center w-full p-3 font-bold text-sm transition ease-in-out duration-200 bg-purple-200 hover:bg-purple-300">SEP</button>
-          <button className="flex items-center justify-center w-full p-3 font-bold text-sm transition ease-in-out duration-200 bg-purple-200 hover:bg-purple-300">OCT</button>
-          <button className="flex items-center justify-center w-full p-3 font-bold text-sm transition ease-in-out duration-200 bg-purple-200 hover:bg-purple-300">NOV</button>
-          <button className="flex items-center justify-center w-full p-3 font-bold text-sm transition ease-in-out duration-200 bg-purple-200 hover:bg-purple-300">DEC</button>
+        <div className="grid grid-cols-4 gap-1 text-gray-600 dark:text-purple-400">
+          <button className="flex items-center justify-center w-full p-3 font-bold text-sm transition ease-in-out duration-200 bg-purple-200 hover:bg-purple-300 dark:bg-gray-900 hover:dark:bg-gray-700">JAN</button>
+          <button className="flex items-center justify-center w-full p-3 font-bold text-sm transition ease-in-out duration-200 bg-purple-200 hover:bg-purple-300 dark:bg-gray-900 hover:dark:bg-gray-700">FEB</button>
+          <button className="flex items-center justify-center w-full p-3 font-bold text-sm transition ease-in-out duration-200 bg-purple-200 hover:bg-purple-300 dark:bg-gray-900 hover:dark:bg-gray-700">MAR</button>
+          <button className="flex items-center justify-center w-full p-3 font-bold text-sm transition ease-in-out duration-200 bg-purple-200 hover:bg-purple-300 dark:bg-gray-900 hover:dark:bg-gray-700">APR</button>
+          <button className="flex items-center justify-center w-full p-3 font-bold text-sm transition ease-in-out duration-200 bg-purple-200 hover:bg-purple-300 dark:bg-gray-900 hover:dark:bg-gray-700">MAY</button>
+          <button className="flex items-center justify-center w-full p-3 font-bold text-sm transition ease-in-out duration-200 bg-purple-200 hover:bg-purple-300 dark:bg-gray-900 hover:dark:bg-gray-700">JUN</button>
+          <button className="flex items-center justify-center w-full p-3 font-bold text-sm transition ease-in-out duration-200 bg-purple-200 hover:bg-purple-300 dark:bg-gray-900 hover:dark:bg-gray-700">JUL</button>
+          <button className="flex items-center justify-center w-full p-3 font-bold text-sm transition ease-in-out duration-200 bg-purple-200 hover:bg-purple-300 dark:bg-gray-900 hover:dark:bg-gray-700">AUG</button>
+          <button className="flex items-center justify-center w-full p-3 font-bold text-sm transition ease-in-out duration-200 bg-purple-200 hover:bg-purple-300 dark:bg-gray-900 hover:dark:bg-gray-700">SEP</button>
+          <button className="flex items-center justify-center w-full p-3 font-bold text-sm transition ease-in-out duration-200 bg-purple-200 hover:bg-purple-300 dark:bg-gray-900 hover:dark:bg-gray-700">OCT</button>
+          <button className="flex items-center justify-center w-full p-3 font-bold text-sm transition ease-in-out duration-200 bg-purple-200 hover:bg-purple-300 dark:bg-gray-900 hover:dark:bg-gray-700">NOV</button>
+          <button className="flex items-center justify-center w-full p-3 font-bold text-sm transition ease-in-out duration-200 bg-purple-200 hover:bg-purple-300 dark:bg-gray-900 hover:dark:bg-gray-700">DEC</button>
         </div>
       </div>
       <div className="flex items-center justify-center w-full">
